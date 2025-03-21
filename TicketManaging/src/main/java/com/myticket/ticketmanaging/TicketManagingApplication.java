47341a3f9cda894859930feab1701b49a29ac27b
paka-ops <pakawess@gmail.com>
2025-03-21 07:55:39 -0700

    Ticket managing project

---
+++ TicketManaging/src/main/java/com/myticket/ticketmanaging/TicketManagingApplication.java
@@ -0,0 +1,13 @@
+package com.myticket.ticketmanaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagingApplication.class, args);
+	}
+
+}
---
+++ TicketManaging/src/test/java/com/myticket/ticketmanaging/TicketManagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myticket.ticketmanaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketManagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
